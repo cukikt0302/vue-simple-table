b135db8c40cff0e9719b8980cc4a3d193ad92cfa
cukikt0302 <cukikt0302@gmail.com>
2017-04-14 16:01:34 +0700

    Add files via upload

---
+++ general.vue
@@ -0,0 +1,191 @@
+<template>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="box box-primary">
+				<div class="box-body no-padding table-responsive">
+					<my-table ref="my_table"
+						:header="header" :body="body" :data="data" :action="action"
+						:config="{table_class: 'table table-striped', checkbox: true, edit_row: true}" 
+						@edit="edit" checkbox_method="set_checkbox" @set_checkbox="set_checkbox">
+						
+						<template slot="edit_row" scope="res">
+							<form @submit.prevent="" role="form" @keyup.esc="$refs.my_table.cancel_edit_row">
+
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="title">Title</label>
+										<input type="text" class="form-control input-sm" v-focus 
+										id="title" placeholder="Title" :value="res.row.title">
+									</div>
+								
+									<div class="form-group">
+										<label for="def">Def</label>
+										<input type="text" class="form-control input-sm" 
+										id="def" placeholder="Input field">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<label for="gha">Gha</label>
+										<input type="text" class="form-control input-sm" 
+										id="gha" placeholder="Title">
+									</div>
+								
+									<div class="form-group">
+										<label for="category">Category</label>
+										<select name="category" id="category" class="form-control input-sm">
+											<option value="Default">Default</option>
+											<option value="Internet">Internet</option>
+											<option value="Cooking">Cooking</option>
+										</select>
+									</div>
+								</div>
+							
+								<div class="col-md-12">
+									<button type="submit" class="btn btn-flat btn-sm btn-primary">Submit</button>
+									<button type="button" class="btn btn-flat btn-sm btn-default" 
+									@click.prevent="$refs.my_table.cancel_edit_row()">Cancel</button>
+								</div>
+							</form>
+						</template>
+
+						<template slot="input" scope="res">
+							<input type="text" v-focus v-selected v-model="action.edit_value"
+							class="input-sm form-control" @blur="reset_edit(res)" 
+							@keydown.enter="submit(res)" @keydown.esc="reset_edit(res)">
+						</template>
+
+						<template slot="select" scope="res">
+							<select v-focus class="input-sm form-control" 
+							v-model="action.edit_value" @blur="submit(res)"
+							@keydown.enter="submit(res)" @keydown.esc="reset_edit(res)">
+								<option :value="'Default'">Default</option>
+								<option :value="'Internet'">Internet</option>
+								<option :value="'Cooking'">Cooking</option>
+							</select>
+						</template>
+
+						<template slot="textarea" scope="res">
+							<textarea rows="1" v-focus class="input-sm form-control" 
+							@blur="reset_edit(res)" @keydown.enter="submit(res)"
+							@keydown.esc="reset_edit(res)" v-model="action.edit_value"></textarea>
+						</template>
+
+					</my-table>
+
+					<pre>{{ $data.action }}</pre>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		name: 'post-general',
+
+		data() {
+			return {
+				header: [
+					{ title: 'ID', class: 'text-center' },
+					{ title: 'Title' },
+					{ title: 'Category' },
+					{ title: 'Create', class: 'text-center' },
+					{ title: 'Author' }
+				],
+
+				body: [
+					{ class: 'text-center' },
+					{ method: {dblclick: 'edit', slot: 'textarea'}  },
+					{ method: {dblclick: 'edit', slot: 'select'} },
+				 	{ class: 'text-center', method: {dblclick: 'edit', slot: 'input'} },
+					{ method: {dblclick: 'edit', slot: 'input'} }
+				],
+
+				data: [
+					{
+						id: 1,
+						title: 'Lorem ipsum dolor sit amet',
+						category: 'Internet',
+						create: '03/02/2017',
+						author: 'administrator'
+					},
+					{
+						id: 2,
+						title: 'Esse explicabo, beatae accusantium odit ipsa velit atque',
+						category: 'Cooking',
+						create: '15/09/2017',
+						author: 'moderator'
+					},
+					{
+						id: 3,
+						title: 'Lorem ipsum dolor sit amet',
+						category: 'Default',
+						create: '01/04/2014',
+						author: 'guest'
+					},
+					{
+						id: 4,
+						title: 'Esse explicabo, beatae accusantium odit ipsa velit atque',
+						category: 'Internet',
+						create: '28/11/1986',
+						author: 'alibaba'
+					}
+				],
+
+				action: {
+					r: false,// row index
+					c: false,// col index
+					edit_value: '',// v-model for all edit model
+					checkbox: [],
+					edit_row: {// value from edit_row my-table component
+						title: '',
+						category: '',
+						create: '',
+						author: ''
+					}
+				}
+			}
+		},
+
+		methods: {
+
+			edit(row, r, c, k) {
+				this.action.r = r;
+				this.action.c = c;
+				this.action.edit_value = row[k];
+			},
+
+			submit(res) {
+				if (res.row[res.k] != this.action.edit_value)
+				{
+					res.row[res.k] = this.action.edit_value;
+				}
+				this.action.r = false;
+				this.action.c = false;
+			},
+
+			reset_edit(res) {
+				this.action.r = false;
+				this.action.c = false;
+			},
+
+			check_ev(item, str) {
+				return Object.keys(item)[0] == str 
+					? item[Object.keys(item)]
+					: false;
+			},
+
+			set_checkbox(item) {
+				this.action.checkbox = item
+			}
+
+		},
+
+		components: {
+			'my-table': require('table.vue')
+		}
+	}
+</script>
---
+++ table.vue
@@ -0,0 +1,210 @@
+<template>
+	<div>
+		<table :class="config.table_class">
+			
+			<thead>
+				<tr>
+					<th class="text-capitalize" v-for="(thead, k, i) in header" :class="thead.class">
+						<span v-if="config.checkbox && k === 0">
+							<input type="checkbox" v-model="checkall">
+						</span>
+						<span v-else>{{ thead.title }}</span>
+					</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr v-for="(row, r) in data" :class="{'tr-active': context_menu_row.id === row.id}"
+				@contextmenu.prevent="$refs.ctxMenu.open($event, row)"
+				v-if="row.id !== toggle_edit_row">
+
+					<td v-for="(item, k, c) in row" :key="item.id" :class="body[c].class">
+
+						<div v-if="body[c].hasOwnProperty('method') && has_ev(body[c].method)">
+
+							<span v-if="check_ev(body[c].method, 'click')" 
+							@click="click(check_ev(body[c].method, 'click'), row, r, c, k)">
+								{{ item }}
+							</span>
+
+							<span v-else-if="check_ev(body[c].method, 'dblclick')"
+							@dblclick="dblclick(check_ev(body[c].method, 'dblclick'), row, r, c, k)">
+								<span v-if="action.r === r && action.c === c">
+
+									<slot v-if="check_slot(body[c].method)" 
+									:name="check_slot(body[c].method)"
+									:row="row" :r="r" :c="c" :k="k"></slot>
+
+									<span v-else>Can't find <b>Slot</b></span>
+
+								</span>
+
+								<span v-else>{{ item }}</span>
+							</span>
+
+							<span v-else-if="check_ev(body[c].method, 'hover')"
+							@mouseover="hover(check_ev(body[c].method, 'hover'), row, r, c, k)">
+								{{ item }}
+							</span>
+
+						</div>
+
+						<div v-else>
+							<span v-if="config.checkbox && k == 'id' && c === 0">
+								<input type="checkbox" :value="item" v-model="checkbox">
+							</span>
+							<span v-else>{{ item }}</span>
+						</div>
+
+					</td>
+
+				</tr>
+
+				<tr v-else class="tr-active">
+					<td :colspan="header.length">
+						<slot name="edit_row" :row="find_by_id(toggle_edit_row)"></slot>
+					</td>
+				</tr>
+			</tbody>
+
+		</table>
+
+		<context-menu v-if="config.edit_row" id="context-menu" ref="ctxMenu"
+		@ctx-open="onCtxOpen" @ctx-cancel="resetCtxLocals" @ctx-close="onCtxClose">
+		  <li @click.prevent="set_edit_row">Edit</li>
+		  <li @click.prevent="remove_row">Delete</li>
+		  <li>Not yet</li>
+		</context-menu>
+
+		<context-menu v-else style="display: none" id="context-menu" ref="ctxMenu"></context-menu>
+	</div>
+</template>
+
+<style>
+	.tr-active {
+		border-left: 1px solid #1a2226;
+	}
+
+	.ctx-menu { font-size: 1em !important; }
+	#context-menu li {
+		padding: 3px 20px;
+		font-size: .9em;
+		font-weight: 600;
+    line-height: 1.5;
+    cursor: context-menu;
+	}
+	#context-menu li:hover {
+		color: #2b2d2f;
+    text-decoration: none;
+    background-color: #f5f5f5;
+	}
+</style>
+
+<script>
+	export default {
+		name: 'post-general',
+
+		props: {
+			checkbox_method: { default() { return '' } },
+			checkall_method: { default() { return '' } },
+			config: { default() { return false } },
+			header: { default() { return {} } },
+			body: { default() { return {} } },
+			action: { default() { return {} } },
+			data: { default() { return {} } }
+		},
+
+		data() {
+			return {
+				checkbox: [],
+				context_menu_row: '',
+				toggle_edit_row: false
+			}
+		},
+
+		computed: {
+      checkall: {
+        get: function () {
+          return this.data 
+          	? this.checkbox.length == this.data.length 
+          	: false;
+        },
+        set: function (value) {
+          var checkbox = [];
+
+          if (value) {
+            this.data.forEach(function (item) {
+              checkbox.push(item.id);
+            });
+          }
+
+          this.checkbox = checkbox;
+        }
+      }
+    },
+
+		watch: {
+			'checkbox': function(newVal) {
+				this.$emit(this.checkbox_method, newVal)
+			}
+		},
+
+		methods: {
+			has_ev(item) {
+				if (item['click'] != undefined)
+				{
+					return true;
+				} else if (item['click'] != undefined) {
+					return true;
+				} else if (item['dblclick'] != undefined) {
+					return true;
+				} else if (item['hover'] != undefined) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+
+			check_ev(item, str) {
+				return Object.keys(item)[0] == str 
+					? item[Object.keys(item)[0]]
+					: false;
+			},
+
+			check_slot(item) {
+				return Object.keys(item)[1] == 'slot' 
+					? item[Object.keys(item)[1]]
+					: false;
+			},
+
+			click(Emit, row, r, c, k) { this.$emit(Emit, row, r, c, k); },
+			dblclick(Emit, row, r, c, k) { this.$emit(Emit, row, r, c, k); },
+			hover(Emit, row, r, c, k) { this.$emit(Emit, row, r, c, k); },
+
+			onCtxOpen(row) { this.context_menu_row = row },
+      onCtxClose(row) { },
+      resetCtxLocals(row) { this.context_menu_row = {} },
+
+      set_edit_row() {// context_menu_row when right click context menu onCtxOpen()
+      	this.toggle_edit_row = this.context_menu_row.id
+      },
+
+      cancel_edit_row() { 
+      	this.toggle_edit_row = false
+      	this.context_menu_row = {}
+      },
+
+      remove_row() {
+      	let index = this.data.indexOf(this.context_menu_row)
+      	if (confirm(`Are you sure remove row: ${index + 1}`))
+      	this.data.splice(index, 1)
+      },
+
+      find_by_id(id) { return this.data.find(obj => obj.id === id) }
+		},
+
+		components: {
+			'context-menu': require('vue-context-menu')
+		}
+	}
+</script>
