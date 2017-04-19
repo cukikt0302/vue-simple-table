e884aedd80c23440ebd18935ed78c8a2cbd968ad
Nguyen Ke Nhat <cukikt0302@gmail.com>
2017-04-19 17:46:08 +0700

    Update pagination.vue

--- pagination.vue
+++ pagination.vue
@@ -1,140 +1,140 @@
 <template>
-	<div>
-		<ul :class="ul_class" v-if="total_page*per_page*current_page > 0">
-			
-			<li :disabled="!showFirst" :class="{'disabled' : !showFirst}">
-				<a @click.prevent="firstPage" href="#">
-					<slot v-if="$slots.firstPage" name="firstPage"></slot>
-					<span v-else>First</span>
-				</a>
-			</li>
+<div>
+  <ul :class="ul_class" v-if="total_page*per_page*current_page > 0">
 
-			<li :disabled="!showPrev" :class="{'disabled' : !showPrev}">
-				<a @click.prevent="prevPage" href="#">
-					<slot v-if="$slots.prevPage" name="prevPage"></slot>
-					<span v-else>{{ current_page - 1}}</span>
-				</a>
-			</li>
+    <li :disabled="!showFirst" :class="{'disabled' : !showFirst}">
+      <a @click.prevent="firstPage" href="#">
+        <slot v-if="$slots.firstPage" name="firstPage"></slot>
+        <span v-else>First</span>
+      </a>
+    </li>
 
-			<li class="active" :class="current" @click="set_toggle_input()">
-				<span style="position: relative;">{{ current_page }}</span>
-				<div v-if="toggle_input" :style="`position: absolute; ${placement}: -10px;`">
-					
-					<form @submit.prevent="change_current_page()">
-						<input type="number" v-focus v-selected 
-							@blur="toggle_input = false" @keydown.esc="toggle_input = false"
-							style="width: 80px; display: inline-block;" 
-							required min="1" :max="total_page"
-							:value="current_page" v-model.number="input_value">
-							<small style="color: auto;">Max: {{ total_page }}</small>
-					</form>
+    <li :disabled="!showPrev" :class="{'disabled' : !showPrev}">
+      <a @click.prevent="prevPage" href="#">
+        <slot v-if="$slots.prevPage" name="prevPage"></slot>
+        <span v-else>{{ current_page - 1}}</span>
+      </a>
+    </li>
 
-				</div>
-			</li>
+    <li class="active" :class="current" @click="set_toggle_input()">
+      <span style="position: relative;">{{ current_page }}</span>
+      <div v-if="toggle_input" :style="`position: absolute; ${placement}: -10px;`">
 
-			<li :disabled="!showNext" :class="{'disabled' : !showNext}">
-				<a @click.prevent="nextPage" href="#">
-					<slot v-if="$slots.nextPage" name="nextPage"></slot>
-					<span v-else>{{ current_page + 1}}</span>
-				</a>
-			</li>
+        <form @submit.prevent="change_current_page()">
+          <input type="number" v-focus v-selected 
+            @blur="toggle_input = false" @keydown.esc="toggle_input = false"
+            style="width: 80px; display: inline-block;" 
+            required min="1" :max="total_page"
+            :value="current_page" v-model.number="input_value">
+            <small style="color: auto;">Max: {{ total_page }}</small>
+        </form>
 
-			<li :disabled="!showLast" :class="{'disabled' : !showLast}">
-				<a @click.prevent="lastPage" href="#">
-					<slot v-if="$slots.lastPage" name="lastPage"></slot>
-					<span v-else>Last</span>
-				</a>
-			</li>
+      </div>
+    </li>
 
-		</ul>
-	</div>
+    <li :disabled="!showNext" :class="{'disabled' : !showNext}">
+      <a @click.prevent="nextPage" href="#">
+        <slot v-if="$slots.nextPage" name="nextPage"></slot>
+        <span v-else>{{ current_page + 1}}</span>
+      </a>
+    </li>
+
+    <li :disabled="!showLast" :class="{'disabled' : !showLast}">
+      <a @click.prevent="lastPage" href="#">
+        <slot v-if="$slots.lastPage" name="lastPage"></slot>
+        <span v-else>Last</span>
+      </a>
+    </li>
+
+  </ul>
+</div>
 </template>
 
 <script>
 export default {
-	name: 'pagination',
-	
-	props: {
-		placement: {// top or bottom
-			default() { return 'bottom' }
-		},
-		ul_class: {
-			default() { return 'pagination pagination-sm' }
-		},
-		current: {
-			default() { return 'active' }
-		},
-		total_page: {
-			default() { return 0} 
-		},
-		per_page: {
-			default() { return 0 } 
-		}
-	},
+  name: 'pagination',
 
-	data() {
-		return {
-			current_page: 1,
-			toggle_input: false,
-			input_value: ''
-		}
-	},
+  props: {
+    placement: {// top or bottom
+      default() { return 'bottom' }
+    },
+    ul_class: {
+      default() { return 'pagination pagination-sm' }
+    },
+    current: {
+      default() { return 'active' }
+    },
+    total_page: {
+      default() { return 0} 
+    },
+    per_page: {
+      default() { return 0 } 
+    }
+  },
 
-	computed: {
-		showNext() {
-			return this.current_page < this.total_page
-		},
+  data() {
+    return {
+      current_page: 1,
+      toggle_input: false,
+      input_value: ''
+    }
+  },
 
-		showPrev() {
-			return this.current_page > 1
-		},
+  computed: {
+    showNext() {
+      return this.current_page < this.total_page
+    },
 
-		showFirst() {
-			return (this.current_page - 1) > 1
-		},
+    showPrev() {
+      return this.current_page > 1
+    },
 
-		showLast() {
-			return (this.current_page + 1) < this.total_page
-		},
-	},
+    showFirst() {
+      return (this.current_page - 1) > 1
+    },
 
-	methods: {
-		nextPage() {
-			if (this.showNext)
-				this.current_page += 1
-		},
+    showLast() {
+      return (this.current_page + 1) < this.total_page
+    },
+  },
 
-		prevPage() {
-			if (this.showPrev)
-				this.current_page -= 1
-		},
+  methods: {
+    nextPage() {
+      if (this.showNext)
+        this.current_page += 1
+    },
 
-		firstPage() {
-			if (this.showFirst)
-				this.current_page = 1
-		},
+    prevPage() {
+      if (this.showPrev)
+        this.current_page -= 1
+    },
 
-		lastPage() {
-			if (this.showLast)
-				this.current_page = this.total_page
-		},
+    firstPage() {
+      if (this.showFirst)
+        this.current_page = 1
+    },
 
-		set_toggle_input() {
-			this.input_value = this.current_page
-			this.toggle_input = !this.toggle_input
-		},
+    lastPage() {
+      if (this.showLast)
+        this.current_page = this.total_page
+    },
 
-		change_current_page() {
-			this.toggle_input = false
-			if (this.current_page !== this.input_value)
-			this.current_page = this.input_value
-		}
-	},
+    set_toggle_input() {
+      this.input_value = this.current_page
+      this.toggle_input = !this.toggle_input
+    },
 
-	watch: {
-		current_page(newVal) {
-			this.$emit('current_page', newVal)
-		}
-	}
+    change_current_page() {
+      this.toggle_input = false
+      if (this.current_page !== this.input_value)
+      this.current_page = this.input_value
+    }
+  },
+
+  watch: {
+    current_page(newVal) {
+      this.$emit('current_page', newVal)
+    }
+  }
 }
 </script>
